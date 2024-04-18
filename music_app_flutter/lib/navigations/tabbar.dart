c14f51e6c881fcbe9648a9faf4f6d0f3528d3d6e
lam4est <lam058b@gmail.com>
2024-04-18 22:10:23 +0700

    first

---
+++ music_app_flutter/lib/navigations/tabbar.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:music_app_flutter/views/home.dart';
+
+class Tabbar extends StatefulWidget {
+  const Tabbar({super.key});
+
+  @override
+  State<Tabbar> createState() => _TabbarState();
+}
+
+class _TabbarState extends State<Tabbar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home_outlined),
+          label: "Home",
+          ),
+
+          BottomNavigationBarItem(
+          icon: Icon(Icons.home_outlined),
+          label: "Home",
+          ),
+
+          BottomNavigationBarItem(
+          icon: Icon(Icons.home_outlined),
+          label: "Home",
+          ),
+
+          BottomNavigationBarItem(
+          icon: Icon(Icons.home_outlined),
+          label: "Home",
+          ),
+
+          BottomNavigationBarItem(
+          icon: Icon(Icons.home_outlined),
+          label: "Home",
+          ),
+
+          BottomNavigationBarItem(
+          icon: Icon(Icons.home_outlined),
+          label: "Home",
+          ),
+          
+        
+      ],),
+      body: HomeView(),
+    );
+  }
+}
