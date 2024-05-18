6060186e7d45cdc897f0d3357c0264c3e1095817
lam4est <lam058b@gmail.com>
2024-05-18 13:51:22 +0700

    add login & signup UI/UX (include validation)

--- music_app_flutter/lib/main.dart
+++ music_app_flutter/lib/main.dart
@@ -1,53 +1,88 @@
+// import 'package:flutter/material.dart';
+// import 'package:music_app_flutter/navigations/tabbar.dart';
+// import 'package:music_app_flutter/widgets/music_player.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatefulWidget {
+//   @override
+//   _MyAppState createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       themeMode: ThemeMode.dark,
+//       darkTheme: ThemeData(
+//         scaffoldBackgroundColor: Color.fromRGBO(35, 35, 35, 0),
+//         brightness: Brightness.dark,
+//         bottomNavigationBarTheme: BottomNavigationBarThemeData(
+//           backgroundColor: Color.fromRGBO(211, 203, 203, 0),
+//           type: BottomNavigationBarType.fixed,
+//           selectedLabelStyle: TextStyle(
+//             fontSize: 12,
+//           ),
+//           unselectedLabelStyle: TextStyle(
+//             fontSize: 12,
+//           ),
+//           selectedItemColor: Colors.white,
+//           unselectedItemColor: Colors.white38,
+//         ),
+//       ),
+//       home: Scaffold(
+//         body: Stack(
+//           children: [
+//             Positioned.fill(
+//               child: Tabbar(), // Nội dung chính của ứng dụng
+//             ),
+//             Positioned(
+//               bottom: 56.0, // Chiều cao của BottomNavigationBar
+//               left: 0,
+//               right: 0,
+//               child: MusicPlayerWidget(),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
-import 'package:music_app_flutter/navigations/tabbar.dart';
-import 'package:music_app_flutter/widgets/music_player.dart';
+import 'package:flutter/services.dart';
+import 'package:music_app_flutter/authentication/login.dart';
+
 
 void main() {
-  runApp(MyApp());
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarBrightness: Brightness.dark,
+    systemNavigationBarColor: Colors.transparent,
+    systemNavigationBarDividerColor: Colors.transparent,
+    systemNavigationBarIconBrightness: Brightness.dark,
+    statusBarIconBrightness: Brightness.dark,
+  ));
+  runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.dark,
-      darkTheme: ThemeData(
-        scaffoldBackgroundColor: Color.fromRGBO(35, 35, 35, 0),
-        brightness: Brightness.dark,
-        bottomNavigationBarTheme: BottomNavigationBarThemeData(
-          backgroundColor: Color.fromRGBO(211, 203, 203, 0),
-          type: BottomNavigationBarType.fixed,
-          selectedLabelStyle: TextStyle(
-            fontSize: 12,
-          ),
-          unselectedLabelStyle: TextStyle(
-            fontSize: 12,
-          ),
-          selectedItemColor: Colors.white,
-          unselectedItemColor: Colors.white38,
-        ),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      home: Scaffold(
-        body: Stack(
-          children: [
-            Positioned.fill(
-              child: Tabbar(), // Nội dung chính của ứng dụng
-            ),
-            Positioned(
-              bottom: 56.0, // Chiều cao của BottomNavigationBar
-              left: 0,
-              right: 0,
-              child: MusicPlayerWidget(),
-            ),
-          ],
-        ),
-      ),
+      home: const LoginScreen(),
     );
   }
 }
